3efb581ec5a6640311ec451f22d996ddf13f437a
swapnilk06 <127191602+swapnilk06@users.noreply.github.com>
2023-05-31 21:47:19 +0530

    Add files via upload

---
+++ Assignment-1/ContainsDuplicate-Q6.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+      def containsDuplicate(self, nums: List[int]) -> bool:
+
+            hset = set()
+            for idx in nums:
+                  if idx in hset:
+                        return True
+                  else:
+                        hset.add(idx)
