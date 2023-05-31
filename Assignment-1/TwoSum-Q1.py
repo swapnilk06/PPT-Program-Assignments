988b90098e4a9141f3d1c3e0a8040aee8f1b522b
swapnilk06 <127191602+swapnilk06@users.noreply.github.com>
2023-05-31 17:52:43 +0530

    Update TwoSum-Q1.py

--- Assignment-1/TwoSum-Q1.py
+++ Assignment-1/TwoSum-Q1.py
@@ -1,17 +1,14 @@
-# Two numbers Sum
-
-from typing import List
+# Two Numbers Sum
 
 class Solution(object):
-   def twoSum(self, nums, target):
-      
-      
-      required = {}
-      for i in range(len(nums)):
+    def twoSum(self, nums, target):
+         
+        required = {}
+        for i in range(len(nums)):
             if target - nums[i] in required:
-                  return [required[target - nums[i]],i]
+                return [required[target - nums[i]],i]
             else:
-                  required[nums[i]]=i
+                required[nums[i]]=i
 
 input_list = [2,7,11,15]
 obj = Solution()
