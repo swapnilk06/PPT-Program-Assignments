703bde203e986b75780dbe3a5b6e75eeb9b68065
swapnilk06 <127191602+swapnilk06@users.noreply.github.com>
2023-06-02 16:01:02 +0530

    Update SearchInsertPosition-Q4.py

--- Assignment-3/SearchInsertPosition-Q4.py
+++ Assignment-3/SearchInsertPosition-Q4.py
@@ -12,3 +12,15 @@ class Solution:
             else:
                 return mid
         return l
+
+    
+# Algorithm with O(log n) runtime complexity
+
+# 1] Initialize left = 0, which will store the required index
+# 2] right = len(nums) - 1, num = size of the array
+# 3] use while left <= right
+# 4] For middle index mid = left+ right / 2
+# 5] In if case num[mid] < target, then left = mid + 1 and move to the right half using left = mid + 1
+# 6] In Else if block leaves, then num[mid] > target, we move to the left half using right = mid – 1 
+# 7] Else Return mid
+# 8] after while, return left 
